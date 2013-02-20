9878c80a45edb4c77aa36d8464bad2d2a3c1871e
emag <emag@mail.emamotor.org>
2013-02-20 20:50:49 +0900

    easy viewable tree structure

--- src/test/java/org/emamotor/fpforjavadevs/functions/RecursionTest.java
+++ src/test/java/org/emamotor/fpforjavadevs/functions/RecursionTest.java
@@ -26,9 +26,10 @@ public class RecursionTest {
 
     @Test
     public void walkATree() {
-        Tree root = new Tree(new Tree(new Tree(null, 3, null), 2, new Tree(
-                new Tree(null, 5, null), 4, null)), 1, new Tree(new Tree(null,
-                7, null), 6, new Tree(null, 8, null)));
+        Tree root = new Tree(
+                new Tree(new Tree(null, 3, null), 2, new Tree(new Tree(null, 5, null), 4, null)),
+                1,
+                new Tree(new Tree(null, 7, null), 6, new Tree(null, 8, null)));
 
         String expected = "(((^-3-^)-2-((^-5-^)-4-^))-1-((^-7-^)-6-(^-8-^)))";
         assertEquals(expected, root.toString());
